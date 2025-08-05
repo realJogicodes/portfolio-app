709f0c18096dafcaf44897ef85fd2d2762385063
proofofjogi <ingokonito@protonmail.com>
2025-08-05 15:42:32 +1000

    refactor: DT styled in change log

--- src/components/ChangeLog.tsx
+++ src/components/ChangeLog.tsx
@@ -1,3 +1,4 @@
+import DT from './typography/DT';
 import H2 from './typography/H2';
 
 export default function ChangeLog() {
@@ -5,9 +6,9 @@ export default function ChangeLog() {
 		<section className="mt-8">
 			<H2>Changelog</H2>
 			<dl>
-				<dt>August 5th, 2025</dt>
+				<DT>August 5th, 2025</DT>
 				<dd>Refactor: App container, H1, H2 and change log components added. Code made more maintainable.</dd>
-				<dt>August 4th, 2025</dt>
+				<DT>August 4th, 2025</DT>
 				<dd>Project started and hosted on Vercel.</dd>
 			</dl>
 		</section>
---
+++ src/components/typography/DT.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from 'react';
+
+export default function DT({ children }: { children: ReactNode }) {
+	return <dt className="font-semibold">{children}</dt>;
+}
