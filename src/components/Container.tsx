5c683cdc36f47c08c6db602a4ad0b1cc2b0a1b0d
proofofjogi <ingokonito@protonmail.com>
2025-08-05 16:34:29 +1000

    padding added to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
 
 export default function Container({ children }: { children: ReactNode }) {
-	return <div className="container max-w-7xl mx-auto mt-8">{children}</div>;
+	return <div className="container max-w-7xl mx-auto mt-8 px-4">{children}</div>;
 }
