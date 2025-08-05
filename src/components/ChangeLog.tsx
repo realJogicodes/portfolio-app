0d5061170dfc757585c0cc7ba93fc3b0a84f0ea3
proofofjogi <ingokonito@protonmail.com>
2025-08-05 16:37:43 +1000

    change log updated

--- src/components/ChangeLog.tsx
+++ src/components/ChangeLog.tsx
@@ -7,7 +7,10 @@ export default function ChangeLog() {
 			<H2>Changelog</H2>
 			<dl>
 				<DT>August 5th, 2025</DT>
-				<dd>Refactor: App container, H1, H2 and change log components added. Code made more maintainable.</dd>
+				<dd>
+					Refactor: App container, skills section, hero section, external link, H1, H2, DT and change log components added. Code made more maintainable. Display font added,
+					bricolage grotesque FTW.
+				</dd>
 				<DT>August 4th, 2025</DT>
 				<dd>Project started and hosted on Vercel.</dd>
 			</dl>
