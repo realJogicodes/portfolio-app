abc9b46f917dbe7acaadc862bf3e481adf9cc9da
proofofjogi <ingokonito@protonmail.com>
2025-08-05 15:25:44 +1000

    change log explanation improved

--- src/components/ChangeLog.tsx
+++ src/components/ChangeLog.tsx
@@ -5,6 +5,8 @@ export default function ChangeLog() {
 		<section className="mt-8">
 			<H2>Changelog</H2>
 			<dl>
+				<dt>August 5th, 2025</dt>
+				<dd>Refactor: H1, H2 and change log components added. Code made more maintainable.</dd>
 				<dt>August 4th, 2025</dt>
 				<dd>Project started and hosted on Vercel.</dd>
 			</dl>
