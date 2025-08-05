0ad2f89a7434eb239984fefc8da6d0f8d4f49ae2
proofofjogi <ingokonito@protonmail.com>
2025-08-05 15:31:33 +1000

    Skills moved to their own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
 import Container from './components/Container';
 import H1 from './components/headings/H1';
-import H2 from './components/headings/H2';
 import ChangeLog from './components/ChangeLog';
+import Skills from './components/Skills';
 
 function App() {
-	const skills = ['JavaScript', 'Svelte', 'Python', 'Swift', 'TailwindCSS', 'SQLite3', 'PocketBase', 'Supabase'];
 	return (
 		<Container>
 			<main className="flex flex-col items-start gap-3">
@@ -16,18 +15,7 @@ function App() {
 					Hence this page. Building it, I am learning React, and of course I am building this using React.
 				</p>
 			</main>
-			<section className="bg-slate-100 p-4 rounded-2xl flex flex-col items-start gap-4 mt-4">
-				<H2>Skills</H2>
-				<p>
-					Predating this challenge, I know some programming already. I've been writing code for my own needs, the web and for iOS since 2017. I started with python, before learning
-					Flask, Svelte and Swift.
-				</p>
-				<ul className="flex gap-2">
-					{skills.map((skill) => (
-						<li className=" border-1 border-black rounded-full px-2">{skill}</li>
-					))}
-				</ul>
-			</section>
+			<Skills />
 			<ChangeLog />
 		</Container>
 	);
---
+++ src/components/Skills.tsx
@@ -0,0 +1,19 @@
+import H2 from './headings/H2';
+
+export default function Skills() {
+	const skills = ['JavaScript', 'Svelte', 'Python', 'Swift', 'TailwindCSS', 'SQLite3', 'PocketBase', 'Supabase'];
+	return (
+		<section className="bg-slate-100 p-4 rounded-2xl flex flex-col items-start gap-4 mt-4">
+			<H2>Skills</H2>
+			<p>
+				Predating this challenge, I know some programming already. I've been writing code for my own needs, the web and for iOS since 2017. I started with python, before learning
+				Flask, Svelte and Swift.
+			</p>
+			<ul className="flex gap-2">
+				{skills.map((skill) => (
+					<li className=" border-1 border-black rounded-full px-2">{skill}</li>
+				))}
+			</ul>
+		</section>
+	);
+}
