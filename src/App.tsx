f557fa068dd10475991cb67478fda815dc34439e
proofofjogi <ingokonito@protonmail.com>
2025-08-05 16:01:41 +1000

    link to github project folder added

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,16 @@ function App() {
 			<main className="flex flex-col items-start gap-3">
 				<H1>React Portfolio App</H1>
 				<p className="text-2xl">Documenting the journey of learning React</p>
-				<p className="text-lg">
+				<p className="">
 					Hi, I'm Jogi — a web developer who’s pretty handy with Svelte, and now diving into React. I believe the best way to learn is by building real things. That’s exactly what
 					this site is: my React learning journey, turned into a portfolio.
+				</p>
+				<p className="">
+					The code for this React app is available on{' '}
+					<a className="underline text-blue-600" href="https://github.com/realJogicodes/portfolio-app">
+						GitHub
+					</a>
+					.
 				</p>
 			</main>
 			<Skills />
