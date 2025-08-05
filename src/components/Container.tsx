2ac8e92328d1eb61c1b5a44f89e98e1999634e0c
proofofjogi <ingokonito@protonmail.com>
2025-08-05 15:29:43 +1000

    container markup simplified

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,5 @@
 import type { ReactNode } from 'react';
 
 export default function Container({ children }: { children: ReactNode }) {
-	return (
-		<div className="flex justify-center pt-4">
-			<div className="container max-w-7xl">{children}</div>
-		</div>
-	);
+	return <div className="container max-w-7xl mx-auto mt-8">{children}</div>;
 }
