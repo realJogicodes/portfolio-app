07cee484936fa0ace3f1e5782f6a8de3debf1c8a
proofofjogi <ingokonito@protonmail.com>
2025-08-08 17:11:22 +1000

    change log data moved to array and looped

--- src/components/ChangeLog.tsx
+++ src/components/ChangeLog.tsx
@@ -2,21 +2,36 @@ import DT from './typography/DT';
 import H2 from './typography/H2';
 
 export default function ChangeLog() {
+	const changelog = [
+		{
+			date: 'August 8th, 2025',
+			change: 'Avatar image added to hero section.'
+		},
+		{
+			date: 'August 6th, 2025',
+			change: 'Refactor: Social media component added.'
+		},
+		{
+			date: 'August 5th, 2025',
+			change:
+				'Refactor: App container, skills section, hero section, external link, H1, H2, DT and change log components added. Code made more maintainable. Display font added, bricolage grotesque FTW.'
+		},
+		{
+			date: 'August 4th, 2025',
+			change: 'Project started and hosted on Vercel.'
+		}
+	];
+
 	return (
 		<section className="mt-8">
 			<H2>Changelog</H2>
 			<dl>
-				<DT>August 8th, 2025</DT>
-				<dd>Avatar image added to hero section.</dd>
-				<DT>August 6th, 2025</DT>
-				<dd>Refactor: Social media component added.</dd>
-				<DT>August 5th, 2025</DT>
-				<dd>
-					Refactor: App container, skills section, hero section, external link, H1, H2, DT and change log components added. Code made more maintainable. Display font added,
-					bricolage grotesque FTW.
-				</dd>
-				<DT>August 4th, 2025</DT>
-				<dd>Project started and hosted on Vercel.</dd>
+				{changelog.map((item) => (
+					<>
+						<DT>{item.date}</DT>
+						<dd>{item.change}</dd>
+					</>
+				))}
 			</dl>
 		</section>
 	);
